9b47f44b1f4653f53706cda50cbeaca4bbd2167c
Ben W <bwong2819@gmail.com>
2025-09-28 11:33:34 -0700

    Fixes

--- include/server.h
+++ include/server.h
@@ -1,6 +1,9 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
 class Server {
 public:
     Server(int port);      // constructor (takes a port number)
@@ -8,7 +11,7 @@ public:
 
 private:
     int port_;             // which port to listen on
-    int server_fd_;        // the server socket (file descriptor)
+    SOCKET server_fd_;   
 };
 
 #endif
